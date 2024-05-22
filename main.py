a02fdd2b6f6fc716b277ec4fb77af0c602ea28d5
GusYudhi <gusyudhip@gmail.com>
2024-05-22 17:37:46 +0800

    andre: tambahkan fitur baru

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 import img_editor
 
 # Set theme
-sg.theme("DarkBlue")
+sg.theme("DarkTeal9")
 
 # Define layout components
 first_column = [
@@ -19,14 +19,14 @@ first_column = [
 second_column = [
     [sg.Text("Image Input:")],
     [sg.Image(key="ImgInputViewer", size=(350, 350), background_color="black")],
-    [sg.Button("Generate GIF", key="GenerateGIF")],
 ]
 
 third_column = [
+    [sg.Button("Toggle Thumbnail", key="ToggleThumbnail")], # Jika ditoggle, gambar output thumbnail akan menggunakan gambar sebenarnya, bukan thumbnail
     [sg.Text("Image Output:")],
     [sg.Image(key="ImgOutputViewer", size=(350, 350), background_color="black")],
     [sg.Text("Filter:")],
-    [sg.Combo(values=["Brightness", "Contrast", "Saturation", "Hue"], key="Filter", readonly=True)],
+    [sg.Combo(values=["Brightness", "Contrast", "Saturation", "Hue", "Sharpness", "Color Balance", "Unsharp Mask", "Gaussian Blur", "Edge Detection"], key="Filter", readonly=True)],
     [sg.Text("Intensity:")],
     [sg.Slider(range=(0, 2), default_value=1, resolution=0.1, orientation="h", size=(20, 15), key="Intensity")],
     [sg.Button("Apply Filter", key="ApplyFilter")],
@@ -72,6 +72,17 @@ def apply_filters(image, layers):
             np_image = np.array(image)
             np_image[..., 0] = (np_image[..., 0] * layer["intensity"]).astype(np.uint8)
             image = Image.fromarray(np_image, 'HSV').convert('RGB')
+        elif layer["type"] == "Sharpness":
+            enhancer = ImageEnhance.Sharpness(image)
+            image = enhancer.enhance(layer["intensity"])
+        elif layer["type"] == "Color Balance":
+            image = img_editor.color_balance(image, layer["intensity"])
+        elif layer["type"] == "Unsharp Mask":
+            image = img_editor.unsharp_mask(image, layer["intensity"])
+        elif layer["type"] == "Gaussian Blur":
+            image = img_editor.gaussian_blur(image, layer["intensity"])
+        elif layer["type"] == "Edge Detection":
+            image = img_editor.edge_detection(image)
     return image
 
 # Function to save history
@@ -82,6 +93,7 @@ def save_history(image, layers, index):
 
 history_index = 0
 history_paths = []
+toogle_thumbnail = False # Use thumbnail by default
 
 while True:
     event, values = window.read()
@@ -99,10 +111,20 @@ while True:
     elif event == "ImgList":
         filename_input = os.path.join(values["ImgFolder"], values["ImgList"][0])
         img_input = Image.open(filename_input)
+        img_output = img_input.copy()
         thumbnail = img_editor.generate_preview(img_input)
         bio = io.BytesIO()
         thumbnail.save(bio, format="PNG")
         window["ImgInputViewer"].update(data=bio.getvalue())
+        if toogle_thumbnail == False:
+            bio = io.BytesIO()
+            thumbnail_out = img_editor.generate_preview(img_output)
+            thumbnail_out.save(bio, format="PNG")
+            window["ImgOutputViewer"].update(data=bio.getvalue())
+        else:
+            bio = io.BytesIO()
+            img_input.save(bio, format="PNG")
+            window["ImgOutputViewer"].update(data=bio.getvalue())
         
         # Reset history
         layers = []
@@ -123,10 +145,15 @@ while True:
                 history_paths.append(history_path)
                 window["HistoryList"].update(history_paths)
                 
-                bio = io.BytesIO()
-                thumbnail_out = img_editor.generate_preview(img_output)
-                thumbnail_out.save(bio, format="PNG")
-                window["ImgOutputViewer"].update(data=bio.getvalue())
+                if toogle_thumbnail == False:
+                    bio = io.BytesIO()
+                    thumbnail_out = img_editor.generate_preview(img_output)
+                    thumbnail_out.save(bio, format="PNG")
+                    window["ImgOutputViewer"].update(data=bio.getvalue())
+                else:
+                    bio = io.BytesIO()
+                    img_output.save(bio, format="PNG")
+                    window["ImgOutputViewer"].update(data=bio.getvalue())
     elif event == "Export":
         if img_input:
             img_output = apply_filters(img_input.copy(), layers)
@@ -142,11 +169,26 @@ while True:
                 last_history_path = history_paths[-1]
                 img_output = Image.open(last_history_path)
                 bio = io.BytesIO()
-                img_output.thumbnail((350, 350))
-                img_output.save(bio, format="PNG")
+                thumbnail_out = img_editor.generate_preview(img_output)
+                thumbnail_out.save(bio, format="PNG")
                 window["ImgOutputViewer"].update(data=bio.getvalue())
             else:
                 window["ImgOutputViewer"].update(data=None)
+
+    elif event == "ToggleThumbnail":
+        toogle_thumbnail = not toogle_thumbnail
+        if img_input is None:
+            continue
+        if toogle_thumbnail == False:
+            img_output = apply_filters(img_input.copy(), layers)
+            bio = io.BytesIO()
+            thumbnail_out = img_editor.generate_preview(img_output)
+            thumbnail_out.save(bio, format="PNG")
+            window["ImgOutputViewer"].update(data=bio.getvalue())
+        else:
+            bio = io.BytesIO()
+            img_output.save(bio, format="PNG")
+            window["ImgOutputViewer"].update(data=bio.getvalue())
 
 window.close()
 # clear history folder
